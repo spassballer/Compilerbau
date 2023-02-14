1ce94a8c6a54bbe6af867e74de030d847906ef6f
Nico <wenzelmannnico1@gmail.com>
2023-02-14 20:04:06 +0100

    Added Operations for char

--- semantic_check/Binary.java
+++ semantic_check/Binary.java
@@ -20,8 +20,8 @@ public class Binary extends Expression {
     @Override
     Type typeCheck(Map<String, Type> localvars, Clars clars) {
         if (exp1.typeCheck(localvars, clars).equals(exp2.typeCheck(localvars, clars))) {
-            if (exp1.typeCheck(localvars, clars).equals(Type.INT) ||
-                    exp1.typeCheck(localvars, clars).equals(Type.STRING)) {
+            if (exp1.typeCheck(localvars, clars).equals(Type.INT)
+                    || exp1.typeCheck(localvars, clars).equals(Type.STRING)) {
                 if (operator.equals("+")) {
                     type = exp1.typeCheck(localvars, clars);
                     return type;
@@ -46,7 +46,14 @@ public class Binary extends Expression {
                     type = Type.BOOLEAN;
                     return type;
                 }
-            } else {
+            }
+            else if (exp1.typeCheck(localvars, clars).equals(Type.CHAR)){
+                if (operator.equals("==") || operator.equals("!=")){
+                    type = Type.BOOLEAN;
+                    return type;
+                }
+            }
+            else {
                 throw new InvalidTypeException("Invalid operator " + operator);
             }
 
