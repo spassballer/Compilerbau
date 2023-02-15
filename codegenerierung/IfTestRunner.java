a3e38964ca0859342acda28ea66420e71c7c8efe
spassballer <spassballer@gmail.com>
2023-02-15 14:45:15 +0100

    Updated IfTest

---
+++ codegenerierung/IfTestRunner.java
@@ -0,0 +1,16 @@
+public class IfTestRunner {
+    public static void main(String[] args) {
+        IfTest ifTest = new IfTest();
+        boolean t = true;
+        boolean f = false;
+
+        System.out.println("------------------------------------------------");
+        System.out.println("If sucessfull: " + (ifTest.onlyIf(t) == t));
+        System.out.println("If/else sucessfull: " + (ifTest.bothIfAndElse(t, t) == t));
+        System.out.println("If/else sucessfull: " + (ifTest.bothIfAndElse(t, f) == f));
+        System.out.println("If/else sucessfull: " + (ifTest.bothIfAndElse(f, t) == t));
+        System.out.println("If/else sucessfull: " + (ifTest.bothIfAndElse(f, f) == f));
+        System.out.println("------------------------------------------------");
+
+    }
+}
--- testfiles/IfTest.java
+++ testfiles/IfTest.java
@@ -1,38 +1,16 @@
 class IfTest {
-    boolean a(boolean a, boolean b){
+    boolean onlyIf(boolean a){
+        if(a){
+            return a;
+        }
+        return !a;
+    }
+
+    boolean bothIfAndElse(boolean a, boolean b){
         if(a == b){
             return a;
         }else {
             return b;
         }
-    }
-
-    boolean b(boolean a, boolean b){
-        if(a != b){
-            return a;
-        }
-        return b;
-    }
-    boolean c(boolean a){
-        if(a){
-            return a;
-        }else {
-            return !a;
-        }
-    }
-
-    int d(int a, int b){
-        if(a <= b){
-            return a;
-        }else {
-            return b;
-        }
-    }
-
-    String e(boolean a){
-        if(!a){
-            return null;
-        }
-        return "3";
     }
 }
